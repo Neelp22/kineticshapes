b0a802fd9dda103a8e13621ed4de886b76083073
Neelp22 <neelp22@gmail.com>
2013-11-05 16:26:16 -0500

    kinetic

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -4,32 +4,34 @@ var app = {
 		
 		app.width = document.width
 		app.height = document.height
+		app.radius = 400
 
-		app.colors = ["#4F4046","#8CBEB2","#EBE4B3","#F3B557","#F05B47","#4F4046","#8CBEB2","#EBE4B3","#F3B557","#F05B47"];
+
+		app.colors = ["#4F4046","#09B5E3","#00E18F","#F32CCF","#FFDE00","#B3FF00","#007BFD","#8A39DC","#FB01FF","#FD012A"];
 
 
 		app.stage = new Kinetic.Stage({
 			container: "main_container",
 			width: app.width,
-			height: app.height
+			height: app.height,
+			radius: app.radius
 		})
 		app.layer = new Kinetic.Layer();
 
-		app.make.squares(app.width,app.height,20)
+		app.make.polygon(app.width,app.height,200,app.radius)
 	},
 
 	make: {
-		circles: function(){
+		polygon: function(width, height, step, radius){
 
-		},
-		squares: function(width,height,step){
-			
 			if (width <= 0 || height <= 0) {
 				app.stage.add(app.layer)
 			} else {
-				var box = new Kinetic.Rect({
-					x: 0,
-					y: 0,
+				var box = new Kinetic.RegularPolygon({
+					sides: Math.floor(Math.random()*10),
+					x: Math.floor(Math.random()*1000),
+					y: Math.floor(Math.random()*1000),
+					radius: radius,
 					width: width,
 					height: height,
 					stroke: "black",
@@ -37,17 +39,36 @@ var app = {
 					fill: app.colors[Math.floor(Math.random()*10)]
 				})
 				app.layer.add(box)
-				return app.make.squares(width-step,height-step,step)
+				return app.make.polygon(width-step,height-step,radius-step, step)
 
-			}
+		}
+		// squares: function(width,height,step){
+			
+		// 	if (width <= 0 || height <= 0) {
+		// 		app.stage.add(app.layer)
+		// 	} else {
+		// 		var box = new Kinetic.Rect({
+		// 			x: 0,
+		// 			y: 0,
+		// 			width: width,
+		// 			height: height,
+		// 			stroke: "black",
+		// 			strokeWidth: 2,
+		// 			fill: app.colors[Math.floor(Math.random()*10)]
+		// 		})
+		// 		app.layer.add(box)
+		// 		return app.make.squares(width-step,height-step,step)
+
+		// 	}
 			
 
 			
 		}
 	}
 
-}
+};
 
 $(function(){
-	app.initialize()
-})
+	setInterval(app.initialize, 1000);
+
+});
